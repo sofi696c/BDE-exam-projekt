2806fb351c1c37a620b6e7b47e2a7ebe106ecfe4
Sofie <soffis02@easv365.dk>
2024-11-07 23:46:07 +0100

    test-file adjustments

--- test/todo-app.js
+++ test/todo-app.js
@@ -4,23 +4,22 @@ fixture("Todo app tests")
     .page("https://sofiefischer.dk/test/todo/");
 
 
-test("Add a todo", async t => {
-    await t
-        .typeText(Selector("#todoInput", "Buy milk"))
-        .click(Selector("#addTodo"))
-        .typeText(Selector("#todoInput", "Mow lawn"))
-        .click(Selector("#addTodo"))
-
-        .expect(Selector("#todoList").childElementCount).eql(2)
-});
-
-test("Delete a todo", async t => {
-    await t
-        .typeText(Selector("#todoInput", "Buy milk"))
-        .click(Selector("#addTodo"))
-        .typeText(Selector("#todoInput", "Mow lawn"))
-        .click(Selector("#addTodo"))
-        .click(Selector("#todoList").child(0).find(".delete"))
-
-        .expect(Selector("#todoList").childElementCount).eql(1)
-});
+    test("Add a todo", async t => {
+        await t
+            .typeText(Selector("#todoInput"), "Buy milk")
+            .click(Selector("#addTodo"))
+            .typeText(Selector("#todoInput"), "Mow lawn")
+            .click(Selector("#addTodo"))
+            .expect(Selector("#todoList").childElementCount).eql(2)
+    });
+    
+    test("Delete a todo", async t => {
+        await t
+            .typeText(Selector("#todoInput"), "Buy milk")
+            .click(Selector("#addTodo"))
+            .typeText(Selector("#todoInput"), "Mow lawn")
+            .click(Selector("#addTodo"))
+            .click(Selector("#todoList").child(0).find(".delete"))
+            .expect(Selector("#todoList").childElementCount).eql(1)
+    });
+    
